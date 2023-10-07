3286585caf371c84aeb60a7f995855d97b3e062a
dizzydwarfus <67429119+dizzydwarfus@users.noreply.github.com>
2023-10-07 23:54:36 +0200

    updated SECData Class and added TickerData class

--- utils/sec-scraper/sec_class.py
+++ utils/sec-scraper/sec_class.py
@@ -3,6 +3,13 @@ import pandas as pd
 import json
 import xml.etree.ElementTree as ET
 from ratelimit import limits, sleep_and_retry
+import logging
+from bs4 import BeautifulSoup
+from tqdm import trange
+
+# Configure the logging settings
+logging.basicConfig(level=logging.DEBUG, filename='sec_logs.log',
+                    filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class SECData:
@@ -38,10 +45,13 @@ class SECData:
     """
 
     BASE_API_URL = "https://data.sec.gov/"
+    BASE_SEC_URL = "https://www.sec.gov/"
+    BASE_DIRECTORY_URL = "https://www.sec.gov/Archives/edgar/data/"
     US_GAAP_TAXONOMY_URL = "https://xbrl.fasb.org/us-gaap/2023/elts/us-gaap-2023.xsd"
     ALLOWED_TAXONOMIES = ['us-gaap', 'ifrs-full', 'dei', 'srt']
-    DIRECTORY_URL = "https://www.sec.gov/Archives/edgar/data/{cik}/{accession_number}/{file_name}"
-    INDEX_URL = "https://www.sec.gov/Archives/edgar/data/{cik}/index.json"
+    INDEX_EXTENSION = ['-index.html', '-index-headers.html']
+    FILE_EXTENSIONS = ['.xsd', '.htm', '_cal.xml',
+                       '_def.xml', '_lab.xml', '_pre.xml', '_htm.xml', '.xml']
 
     def __init__(self, requester_company: str, requester_name: str, requester_email: str, taxonomy: str):
         self.requester_company = requester_company
@@ -56,9 +66,27 @@ class SECData:
         self.cik = self.get_cik_list()
         self.tags = self.get_usgaap_tags()
         if taxonomy not in self.ALLOWED_TAXONOMIES:
-            raise Exception(
+            raise ValueError(
                 f"Taxonomy {taxonomy} is not supported. Please use one of the following taxonomies: {self.ALLOWED_TAXONOMIES}")
         self.taxonomy = taxonomy
+
+    @sleep_and_retry
+    @limits(calls=10, period=1)
+    def rate_limited_request(self, url: str, headers: dict):
+        """Rate limited request to SEC Edgar database.
+
+        Args:
+            url (str): URL to retrieve data from
+            headers (dict): Headers to be used for API calls
+
+        Returns:
+            response: Response from API call
+        """
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            logging.error(f'''
+Request failed at URL: {url}''')
+        return response
 
     def get_cik_list(self):
         """Retrieves the full list of CIK available from SEC database.
@@ -70,12 +98,11 @@ class SECData:
             cik_df: DataFrame containing CIK and ticker
         """
         url = r"https://www.sec.gov/files/company_tickers.json"
-        cik_raw = requests.get(url)
+        cik_raw = self.rate_limited_request(url, self.sec_headers)
         cik_json = cik_raw.json()
         cik_df = pd.DataFrame.from_dict(cik_json).T
         return cik_df
 
-    # Get specific ticker's CIK number
     def get_ticker_cik(self, ticker: str,):
         """Get a specific ticker's CIK number. 
         CIK########## is the entity's 10-digit Central Index Key (CIK).
@@ -96,25 +123,22 @@ class SECData:
         Returns:
             list of tags
         """
-        response = requests.get(xsd_url, headers=self.sec_headers)
+        response = self.rate_limited_request(xsd_url, headers=self.sec_headers)
         xsd_content = response.text
         root = ET.fromstring(xsd_content)
 
         return [element.attrib['name'] for element in root.findall(".//{http://www.w3.org/2001/XMLSchema}element")]
 
-    @sleep_and_retry
-    @limits(calls=10, period=1)
     def get_submissions(self, cik):
         url = f"{self.BASE_API_URL}submissions/CIK{cik}.json"
-        response = requests.get(url, headers=self.sec_data_headers)
+        response = self.rate_limited_request(
+            url, headers=self.sec_data_headers)
         if response.status_code != 200:
             raise Exception(
                 f"Failed to retrieve submissions for CIK {cik}. Status code: {response.status_code}")
         data = json.loads(response.text)
         return data
 
-    @sleep_and_retry
-    @limits(calls=10, period=1)
     def get_company_concept(self, cik: str, tag: str, taxonomy: str = 'us-gaap',):
         """The company-concept API returns all the XBRL disclosures from a single company (CIK) 
         and concept (a taxonomy and tag) into a single JSON file, with a separate array of facts 
@@ -133,23 +157,21 @@ class SECData:
             data: JSON file containing all the XBRL disclosures from a single company (CIK)
         """
         url = f"{self.BASE_API_URL}api/xbrl/companyconcept/CIK{cik}/{taxonomy}/{tag}.json"
-        response = requests.get(url, headers=self.sec_data_headers)
+        response = self.rate_limited_request(
+            url, headers=self.sec_data_headers)
         data = json.loads(response.text)
         return data
 
-    @sleep_and_retry
-    @limits(calls=10, period=1)
     def get_company_facts(self, cik):
         url = f"{self.BASE_API_URL}api/xbrl/companyfacts/CIK{cik}.json"
-        response = requests.get(url, headers=self.sec_data_headers)
+        response = self.rate_limited_request(
+            url, headers=self.sec_data_headers)
         if response.status_code != 200:
             raise Exception(
                 f"Failed to retrieve company facts for CIK {cik}. Status code: {response.status_code}")
         data = json.loads(response.text)
         return data
 
-    @sleep_and_retry
-    @limits(calls=10, period=1)
     def get_frames(self, taxonomy, tag, unit, period):
         """The xbrl/frames API aggregates one fact for each reporting entity that is last filed that most closely fits the calendrical period requested. 
         This API supports for annual, quarterly and instantaneous data: https://data.sec.gov/api/xbrl/frames/us-gaap/AccountsPayableCurrent/USD/CY2019Q1I.json
@@ -167,7 +189,8 @@ class SECData:
             data: json formatted response
         """
         url = f"{self.BASE_API_URL}api/xbrl/frames/{taxonomy}/{tag}/{unit}/{period}.json"
-        response = requests.get(url, headers=self.sec_data_headers)
+        response = self.rate_limited_request(
+            url, headers=self.sec_data_headers)
         data = json.loads(response.text)
         return data
 
@@ -198,9 +221,7 @@ class SECData:
                               ).dt.days.div(30.4375).round(0)
         return df
 
-    @sleep_and_retry
-    @limits(calls=10, period=1)
-    def get_cik_index(self, cik: str,):
+    def get_index(self, cik: str = None,) -> dict:
         """Each CIK directory and all child subdirectories contain three files to assist in 
         automated crawling of these directories. 
         These are not visible through directory browsing.
@@ -214,9 +235,187 @@ class SECData:
         Returns:
             json: pandas dataframe containing the XBRL disclosures from a single company (CIK)
         """
-        url = self.INDEX_URL.format(cik=cik)
-        response = requests.get(url, headers=self.sec_headers)
+        if cik is not None:
+            url = self.BASE_DIRECTORY_URL + cik + '/' + 'index.json'
+
+        else:
+            url = self.BASE_DIRECTORY_URL + self.cik + '/' + 'index.json'
+
+        response = self.rate_limited_request(url, headers=self.sec_headers)
         return response.json()
+
+
+class TickerData(SECData):
+    """Inherited from SECData class. Retrieves data from SEC Edgar database based on ticker.
+    url is constructed based on the following: https://www.sec.gov/Archives/edgar/data/{cik}/{ascension_number}/{file_name}
+    cik is the CIK number of the company = access via get_ticker_cik
+    ascension_number is the accessionNumber column of filings_df
+    file name for xml is always '{ticker}-{reportDate}.{extension}
+    """
+
+    def __init__(self, ticker: str, requester_company: str = 'Financial API', requester_name: str = 'API Caller', requester_email: str = 'apicaller@gmail.com', taxonomy: str = 'us-gaap',):
+        super().__init__(requester_company, requester_name, requester_email, taxonomy)
+        self.ticker = ticker.upper()
+        self.cik = self.get_ticker_cik(self.ticker)
+        self._submissions = None
+        self._filings = None
+        self.forms = self.filings['form'].unique()
+        self._index = self.get_index(self.cik)
+        self._filing_folder_urls = None
+        self._filing_urls = None
+
+    @property
+    def submissions(self,) -> dict:
+        if self._submissions is None:
+            self._submissions = self.get_submissions(self.cik)
+        return self._submissions
+
+    @property
+    def filings(self,) -> pd.DataFrame:
+        if self._filings is None:
+            self._filings = self.get_filings(self.submissions)
+        return self._filings
+
+    @property
+    def filing_folder_urls(self,) -> list:
+        if self._filing_folder_urls is None:
+            self._filing_folder_urls = self.get_filing_folder_urls()
+        return self._filing_folder_urls
+
+    @property
+    def filing_urls(self,) -> list:
+        if self._filing_urls is None:
+            self._filing_urls = self.get_filing_urls()
+
+        return self._filing_urls
+
+    def get_filing_folder_urls(self,) -> list:
+        """Get filing folder urls from index dict.
+
+        Args:
+            index (dict): index dict from get_index method
+
+        Returns:
+            filing_folder_urls (list): list of filing folder urls
+        """
+        filing_folder_urls = [self.BASE_SEC_URL + self._index['directory']['name'] + '/' + folder['name']
+                              for folder in self._index['directory']['item'] if folder['type'] == 'folder.gif']
+        return filing_folder_urls
+
+    def get_filing_urls(self,) -> list:
+        """Get filing urls from filing folder urls.
+
+        Args:
+            filing_folder_urls (list): list of filing folder urls
+
+        Returns:
+            filing_urls (list): list of filing urls to .txt files
+        """
+        filing_urls = []
+        with trange(len(self.filing_folder_urls), desc=f'Instantiating filing urls for {self.ticker}...') as t:
+            for i in t:
+                logging.info(t)
+                try:
+                    soup = self.get_file_data(self.filing_folder_urls[i])
+                    for link in soup.find_all('a'):
+                        if link.get('href').endswith('.txt'):
+                            filing_urls.append(
+                                self.BASE_SEC_URL + link.get('href'))
+                except Exception as e:
+                    logging.error(
+                        f'Failed to instantiate filing urls for {self.ticker}...')
+                    logging.error(e)
+                    t.write(
+                        f'Failed to instantiate filing urls for {self.ticker}...')
+                    continue
+        return filing_urls
+
+    def get_filings(self, submissions: dict):
+        """Get filings from submissions dict.
+
+        Args:
+            submissions (dict): submissions dict from get_submissions method
+
+        Returns:
+            filings (DataFrame): DataFrame containing filings
+        """
+        filings = pd.DataFrame(submissions['filings']['recent'])
+
+        # Convert reportDate, filingDate, acceptanceDateTime columns to datetime
+        filings['reportDate'] = pd.to_datetime(filings['reportDate'])
+        filings['filingDate'] = pd.to_datetime(filings['filingDate'])
+        filings['acceptanceDateTime'] = pd.to_datetime(
+            filings['acceptanceDateTime'])
+        filings['file_url'] = self.BASE_DIRECTORY_URL + self.cik + '/' + \
+            filings['accessionNumber'].str.replace(
+                '-', '') + '/' + filings['accessionNumber'] + '.txt'
+        return filings
+
+    def get_file_data(self, file_url: str) -> BeautifulSoup:
+        """Get file data from file url which can be retrieved by calling self.get_file_url method.
+
+        Args:
+            file_url (str): File url to retrieve data from on the SEC website
+
+        Returns:
+            data: File data as a BeautifulSoup object
+        """
+        data = self.rate_limited_request(
+            url=file_url, headers=self.sec_headers)
+        soup = BeautifulSoup(data.content, "lxml")
+        return soup
+
+    def search_tags(self, soup: BeautifulSoup, pattern: str) -> BeautifulSoup:
+        """Search for tags in BeautifulSoup object.
+
+        Args:
+            soup (BeautifulSoup): BeautifulSoup object
+            pattern (str): pattern to search for
+
+        Returns:
+            soup: BeautifulSoup object
+        """
+        return soup.find_all(pattern)
+
+    def search_context(self, soup: BeautifulSoup) -> pd.DataFrame:
+        """Search for context in company .txt filing. 
+        Context provides information about the entity, segment, and time period for facts in the filing.
+
+        Args:
+            soup (BeautifulSoup): BeautifulSoup object
+
+        Returns:
+            df: DataFrame containing context information with columns 
+            {
+                'contextId': str,
+                'entity': str,
+                'segment': str,
+                'startDate': 'datetime64[ns]',
+                'endDate': 'datetime64[ns]',
+                'instant': 'datetime64[ns]'
+            }
+        """
+        contexts = self.search_tags(soup, '^context$')
+        dict_list = []
+        columns = {'contextId': str, 'entity': str, 'segment': str,
+                   'startDate': 'datetime64[ns]', 'endDate': 'datetime64[ns]', 'instant': 'datetime64[ns]'}
+        for tag in contexts:
+            temp_dict = {}
+            temp_dict['contextId'] = tag.attrs['id']
+            temp_dict['entity'] = tag.find("entity").text.split()[
+                0] if tag.find("entity") is not None else None
+            temp_dict['segment'] = tag.find("segment").text.strip(
+            ) if tag.find("segment") is not None else None
+            temp_dict['startDate'] = tag.find("startdate").text if tag.find(
+                "startdate") is not None else None
+            temp_dict['endDate'] = tag.find("enddate").text if tag.find(
+                "enddate") is not None else None
+            temp_dict['instant'] = tag.find("instant").text if tag.find(
+                "instant") is not None else None
+            dict_list.append(temp_dict)
+
+        df = pd.DataFrame(dict_list, columns=columns.keys()).astype(columns)
+        return df
 
 
 if __name__ == "__main__":
@@ -225,25 +424,33 @@ if __name__ == "__main__":
 
     # Get user input
     while True:
-        ticker = input("Enter ticker: ")
-        if ticker in sec.cik['ticker'].values:
+        choice = input(
+            """
+Choose one of the following options:
+- Enter a ticker symbol to get all facts in SEC database
+- Enter a company name to search for ticker symbols
+- 'list' to list all tickers
+- 'exit' to exit
+
+Your input: """)
+        if choice in sec.cik['ticker'].values:
             companyfacts = sec.get_data_as_dataframe(
-                sec.get_ticker_cik(ticker))
-            file_name = f"data/{ticker}.csv"
+                sec.get_ticker_cik(choice))
+            file_name = f"data/{choice}.csv"
             companyfacts.to_csv(file_name, index=False)
             print(f"Data saved to {file_name}")
             print('---------------------------------------')
 
-        elif ticker == 'exit':
+        elif choice == 'exit':
             exit()
-        elif ticker == 'list':
+        elif choice == 'list':
             for row in sec.cik.iterrows():
                 print(row[1]['ticker'], row[1]['title'])
             print('---------------------------------------')
 
-        elif ticker.isalpha():
+        elif choice.isalpha():
             search_results = sec.cik.loc[sec.cik['title'].str.lower().str.contains(
-                ticker)]
+                choice)]
             if len(search_results) == 0:
                 print("No results found.")
                 print('---------------------------------------')
